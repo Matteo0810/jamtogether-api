8f1778f29e701d345b7509ed15dcc4a664979092
matteogaillard <matteo.gaillard@protonmail.com>
2025-01-10 21:15:40 +0100

    de pire en pire

--- server/services/websocket.ts
+++ server/services/websocket.ts
@@ -3,8 +3,8 @@ import http from "http";
 import https from "https";
 import fs from "fs";
 
-const certPath = './test.pem';
-const keyPath = './test_private.pem';
+const certPath = '/root/ballon2zipette-corp/letsencrypt/archive/ballon2zipette.com/fullchain.pem';
+const keyPath = '/root/ballon2zipette-corp/letsencrypt/archive/ballon2zipette.com/privkey.pem';
 
 const useSSL = fs.existsSync(certPath) && fs.existsSync(keyPath);
 
