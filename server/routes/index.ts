6d38a2a34ea4b51eed19757adf601f71ede3420b
matteogaillard <matteo.gaillard@protonmail.com>
2025-01-18 21:37:48 +0100

    chore: add to route infos

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,6 +4,7 @@ import spotify from "./spotify";
 
 import rooms from "./rooms";
 import roomActions from "./room/actions";
+import roomInfos from "./room/infos";
 
 export default (instance: FastifyInstance) => {
     instance.register(spotify, { prefix: "spotify" });
@@ -11,5 +12,6 @@ export default (instance: FastifyInstance) => {
     // rooms and room actions
     instance.register(rooms, { prefix: "rooms" });
     instance.register(roomActions, { prefix: "rooms/actions/:id" });
+    instance.register(roomInfos, { prefix: "rooms/infos/:id" });
 
 }
